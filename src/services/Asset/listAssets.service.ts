f8512b261b0351193d61facfc9287ebb5b82ff3e
Matheus Carneiro <mtscrn21@gmail.com>
2024-04-10 20:03:54 -0300

    Create: Filter user in asset service

--- src/services/Asset/listAssets.service.ts
+++ src/services/Asset/listAssets.service.ts
@@ -2,10 +2,16 @@ import AppDataSource from "../../data-source";
 import Asset from "../../entities/assets.entity";
 import { listCashOperations } from "../../schemas/cash_operation.schema";
 
-export const listAllAssetsService = async () => {
+export const listAllAssetsService = async (userId: string) => {
   const assetRepo = AppDataSource.getRepository(Asset);
 
-  const assets = await assetRepo.find();
+  const assets = await assetRepo.find({
+    where: {
+      user: {
+        id: userId,
+      },
+    },
+  });
 
   const response = listCashOperations.parse(assets);
 
